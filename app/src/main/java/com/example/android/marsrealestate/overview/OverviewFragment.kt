a1d4dfbead653cdb1d53081e8140ccc626ac44a2
ERNOUL <iamkapitaan@gmail.com>
2019-05-17 11:04:10 +0200

    project

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -20,9 +20,13 @@ package com.example.android.marsrealestate.overview
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsApiService
 
 /**
  * This fragment shows the the status of the Mars real-estate web services transaction.
@@ -42,6 +46,7 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
@@ -49,14 +54,32 @@ class OverviewFragment : Fragment() {
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+        binding.photosGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
+            viewModel.displayPropertyDetails(it)
+        })
+
+        viewModel.navigateToSelectedProperty.observe(this, Observer {
+            if (null != it) {
+                this.findNavController().navigate(OverviewFragmentDirections.actionShowDetail(it))
+                viewModel.displayPropertyDetailsComplete()
+            }
+        })
 
         setHasOptionsMenu(true)
         return binding.root
     }
 
-    /**
-     * Inflates the overflow menu that contains filtering options.
-     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel.updateFilter(
+            when (item.itemId) {
+                R.id.show_rent_menu -> MarsApiService.MarsApiFilter.SHOW_RENT
+                R.id.show_buy_menu -> MarsApiService.MarsApiFilter.SHOW_BUY
+                else -> MarsApiService.MarsApiFilter.SHOW_ALL
+            }
+        )
+        return true
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.overflow_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,12 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApiService
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -29,6 +35,21 @@ class OverviewViewModel : ViewModel() {
     // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
 
+    private val _status = MutableLiveData<MarsApiStatus>()
+    val status: LiveData<MarsApiStatus>
+        get() = _status
+
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+    val navigateToSelectedProperty: LiveData<MarsProperty>
+        get() = _navigateToSelectedProperty
+
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
         get() = _response
@@ -37,13 +58,44 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiService.MarsApiFilter.SHOW_ALL)
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
-    private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun displayPropertyDetails(marsProperty: MarsProperty) {
+        _navigateToSelectedProperty.value = marsProperty
+    }
+
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
+    }
+
+    private fun getMarsRealEstateProperties(filter: MarsApiService.MarsApiFilter) {
+        coroutineScope.launch {
+            var getPropertiesDeferred = MarsApiService.MarsApi.retrofitService.getProperties(filter.value)
+
+            try {
+                _status.value = MarsApiStatus.LOADING
+                val listResult =  getPropertiesDeferred.await()
+                _status.value = MarsApiStatus.DONE
+                _properties.value = listResult
+
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
+    }
+
+    fun updateFilter(filter: MarsApiService.MarsApiFilter) {
+        getMarsRealEstateProperties(filter)
     }
 }
+
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
